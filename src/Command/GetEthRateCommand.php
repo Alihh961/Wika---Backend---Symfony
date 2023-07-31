0c40efbdcb7c3b09eff7877c03e1dc9e167e1e63
Alihh961 <hajhassan.ali@outlook.com>
2023-07-31 15:15:59 +0200

    adding a command of eth

---
+++ src/Command/GetEthRateCommand.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Command;
+
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+
+#[AsCommand(
+    name: 'app:get-eth-rate',
+    description: 'Add a short description for your command',
+)]
+class GetEthRateCommand extends Command
+{
+
+    public function __construct(private HttpClientInterface $httpClient)
+    {
+        parent::__construct();
+    }
+
+    protected function configure(): void
+    {
+
+
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $response = $this->httpClient->request("GET" , "https://min-api.cryptocompare.com/data/price?fsym=ETH&tsyms=EUR");
+        $currentPrice = $response->getContent();
+
+        $output->writeln($currentPrice);
+
+
+        return Command::SUCCESS;
+    }
+}
--- webpack.config.js
+++ webpack.config.js
@@ -21,6 +21,7 @@ Encore
    * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
    */
   .addEntry("app", "./assets/app.js")
+  .addEntry("bootstrap", "./node_modules/bootstrap/dist/js/bootstrap.js")
   .addStyleEntry("header", "./assets/styles/partials/header.scss")
   .addStyleEntry("login", "./templates/security/login.scss")
   .addStyleEntry("register", "./templates/registration/register.scss")
