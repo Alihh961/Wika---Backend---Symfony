f7c236f44e4ac9d35eb09fafd9d1ec6b79fefa82
Alihh961 <hajhassan.ali@outlook.com>
2023-08-18 15:29:40 +0200

    addingiconsheader

--- assets/app.js
+++ assets/app.js
@@ -11,6 +11,7 @@ import "./styles/app.scss";
 // start the Stimulus application
 import "./bootstrap";
 
+//importing fontawesome
 import "@fortawesome/fontawesome-free/js/fontawesome";
 import "@fortawesome/fontawesome-free/js/solid";
 import "@fortawesome/fontawesome-free/js/regular";
