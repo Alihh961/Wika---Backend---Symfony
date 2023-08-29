f698cde9c42e53204bad09a7ea72bf24d420c742
Alihh961 <hajhassan.ali@outlook.com>
2023-08-30 00:37:58 +0200

    finshing aside nav

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Security;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 class HomeController extends AbstractController
@@ -14,13 +15,15 @@ class HomeController extends AbstractController
     {
     }
 
+
+
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function index( Security $security): Response
     {
+        $user = $security->getUser();
 
         $response = $this->httpClient->request("GET" , "https://min-api.cryptocompare.com/data/price?fsym=ETH&tsyms=USD,JPY,EUR");
         $prix = $response->getContent();
-
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
             'prix' => (json_decode($prix))->EUR
